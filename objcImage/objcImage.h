f3244754d88ee07909314e352fbed6b12d3b2fa7
vilyever <250450035@163.com>
2016-08-05 13:31:28 +0800

    Initial Commit

---
+++ objcImage/objcImage.h
@@ -0,0 +1,19 @@
+//
+//  objcImage.h
+//  objcImage
+//
+//  Created by FTET on 16/8/5.
+//  Copyright © 2016年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objcImage.
+FOUNDATION_EXPORT double objcImageVersionNumber;
+
+//! Project version string for objcImage.
+FOUNDATION_EXPORT const unsigned char objcImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcImage/PublicHeader.h>
+
+
